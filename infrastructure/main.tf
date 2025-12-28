a7e8bef55142735fd7304db163187980921c9ef8
barnapet <barnapet00@gmail.com>
2025-12-28 12:16:11 +0100

    feat(processor): add silver layer lambda and s3 trigger for automated cleaning

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -96,3 +96,37 @@ resource "aws_iot_topic_rule" "obd_telemetry_rule" {
     }
   }
 }
+
+# --- 6. SILVER PROCESSING LAMBDA ---
+resource "aws_lambda_function" "silver_processor" {
+  filename      = "processor.zip"
+  function_name = "smartdrive-silver-processor"
+  role          = aws_iam_role.lambda_exec_role.arn # Must follow Least Privilege
+  handler       = "index.lambda_handler"
+  runtime       = "python3.9"
+
+  environment {
+    variables = {
+      SILVER_BUCKET = aws_s3_bucket.silver.id
+    }
+  }
+}
+
+# --- 7. S3 EVENT TRIGGER ---
+resource "aws_lambda_permission" "allow_s3_bronze" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.silver_processor.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.bronze.arn
+}
+
+resource "aws_s3_bucket_notification" "bronze_trigger" {
+  bucket = aws_s3_bucket.bronze.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.silver_processor.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+  depends_on = [aws_lambda_permission.allow_s3_bronze]
+}
